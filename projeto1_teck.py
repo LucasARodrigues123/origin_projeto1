30ac3cd35ffa2eec70896e05932b6174a1c00b47
LucasARodrigues123 <esscur1dao@gmail.com>
2023-08-23 20:24:56 -0300

    automatizando a funsão 1

---
+++ projeto1_teck.py
@@ -0,0 +1,6 @@
+n1 = float(str(input('digite O valor em KG ou T ?:')))
+n2 = float(str(input('digite O valor da bobina  ?:')))
+n3 = float(str(input('digite A quantidade de Peças ?:')))
+n4 = (n1/n2)*n3
+print('Ototal de peças é {}'.format(n4))
+
