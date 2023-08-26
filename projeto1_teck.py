5b1d43ca1c5c5047cc50725287658e194ee80ef8
LucasARodrigues123 <esscur1dao@gmail.com>
2023-08-26 08:43:54 -0300

    tornando_o_codigo_mais_legivel

--- projeto1_teck.py
+++ projeto1_teck.py
@@ -1,6 +1,12 @@
-n1 = float(str(input('digite O valor em KG ou T ?:')))
-n2 = float(str(input('digite O valor da bobina  ?:')))
-n3 = float(str(input('digite A quantidade de Peças ?:')))
-n4 = (n1/n2)*n3
-print('Ototal de peças é {}'.format(n4))
+# Criar um programa que calcule a quantidade de pesso de um pedido
+peso_do_tubeti_padrão = float(input('Qual valor do peso do Tubete?:'))
+peso_do_pedido = float(input('Qual valor do peso dos Kilos ou toneladas ?:'))
+peso_da_bobina = float(input('Qual valor do peso da Bobina?:'))
+quantas_peças = float(input('Quantas peças Amaquina Produz?:'))
+peso_do_palete = float(input('Qual è o peso do palete?:'))
+resultado = float (peso_do_pedido / peso_da_bobina)
+Resultadofinal = resultado * quantas_peças
+print('O tamanho do Tubete é {:.2f} \n A quantidade peças  é {:.6f}\n O peso do palete é {:.3f}'.format(peso_do_tubeti_padrão, Resultadofinal, peso_do_palete))
+# criar um  calculo que mostre as condiçoes para ativar variaveis espesificas
 
+# criar um codigo que mostre o resultado final do programa
